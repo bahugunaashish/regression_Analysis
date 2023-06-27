2e0eb5449bf3b0e15cabdc02a8ab4d6498e4eb21
bahugunaashish <47498272+bahugunaashish@users.noreply.github.com>
2023-06-27 15:52:41 +0530

    Update regression.py
    added gmpe plot function

--- regression.py
+++ regression.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import math
 
 data = pd.read_csv('H:/Python projects/regression_analysis/regression_analysis/input_with_Depth.csv')  # Replace 'your_data.csv' with the actual file path or data source
 
@@ -53,7 +54,7 @@ def regression_analysis(inde_paras,depe_para):
     # fig1.savefig('Residual_magnitude.png', dpi = 500)
 
     plt.subplot(1,2,2)
-    plt.scatter( data[['R']], residuals,color='red',edgecolors='black')
+    plt.scatter( data[['R']], residuals, color='red',edgecolors='black')
     plt.axhline(y=0, color='red', linestyle='--')
     plt.axhline(y=0, color='red', linestyle='--')
     plt.axhline(y=std_error, color='green', linestyle='--', label='SE = 0.81')
@@ -66,11 +67,43 @@ def regression_analysis(inde_paras,depe_para):
     #plt.show()
     fig1.savefig('Residual.png', dpi = 500)
     
-    return  #residuals, std_error
+    return  residuals, std_error, coefficients
 
 
 
 Xs = data[['M', 'R','ln R','D']]
 y = data[['ln pga']]  # Taking the natural logarithm of the PGA values
 
-regression_analysis(Xs,y)
+residuals, std_error, coefficients = regression_analysis(Xs,y)
+
+df3 = data[(data['M'] >= 3.) & (data['M'] < 4.)]
+df4 = data[(data['M'] >= 4.) & (data['M'] < 5.)]
+df5 = data[(data['M'] >= 5.) & (data['M'] < 6.)]
+
+def gmpe_plot(df, M, D, coefficients):
+    pga = np.exp(df[['ln pga']])*100*9.81
+    
+    # #GMPE plot
+   # M =3.
+    R = np.arange(0.1, 70., 2.)
+    lnR= np.log(R)
+   # D = 10
+    Y = coefficients[0] + coefficients[1]*M + np.multiply(coefficients[2], R) + np.multiply(coefficients[3],lnR) + coefficients[4]*D
+    Y=np.exp(Y)*100*9.81
+    
+    fig = plt.figure(figsize=(6,4))
+    plt.plot(R, Y, color='red', label = 'M = '+str(M))
+    plt.scatter(df[['R']], pga, color='red',edgecolors='black', label = 'M = '+str(M))
+    plt.yscale("log")
+    plt.legend()
+    fig.savefig(str(M)+'.png', dpi = 500)
+    
+M =np.arange(3, 6, 1)
+D = 10
+for m in M:
+    if m == 3:
+        gmpe_plot(df3, m, D, coefficients)
+    elif m == 4:
+        gmpe_plot(df4, m, D, coefficients)
+    elif m == 5:
+        gmpe_plot(df5, m, D, coefficients)
